5ef91f11a549b5a79c0f46f24e3f3f56308bc307
hondodo <potatokidh@gmail.com>
2018-09-26 13:03:10 +0800

    sync

--- Other/TestFFMPEGSave/main.cpp
+++ Other/TestFFMPEGSave/main.cpp
@@ -142,7 +142,7 @@ static int open_output_file(const char *filename)
                 enc_ctx->pix_fmt = AV_PIX_FMT_YUVJ422P;//encoder->pix_fmts[0];//AV_PIX_FMT_YUVJ420P
                 /* video time_base can be set to whatever is handy andsupported by encoder */
                 enc_ctx->time_base = dec_ctx->time_base;
-                enc_ctx->bit_rate = 1024 * 1024;
+                enc_ctx->bit_rate = 2 * 1024 * 1024;
             } else {
                 enc_ctx->sample_rate = dec_ctx->sample_rate;
                 enc_ctx->channel_layout = dec_ctx->channel_layout;
