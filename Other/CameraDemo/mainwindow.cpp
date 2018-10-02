5b9d31f8c0ea45106af4d442afbe4e6f6b1d5064
hondodo <potatokidh@gmail.com>
2018-10-03 00:38:35 +0800

    menu

--- Other/CameraDemo/mainwindow.cpp
+++ Other/CameraDemo/mainwindow.cpp
@@ -305,6 +305,7 @@ void MainWindow::onCameraControlRequestRemove()
         ui->verticalLayoutCams2->removeWidget(control);
         ui->verticalLayoutCams3->removeWidget(control);
         ui->verticalLayoutCams4->removeWidget(control);
+        existsCameraUrls.removeAll(control->getCameraUrl());
         control->stop();
         control->deleteLater();
         control = Q_NULLPTR;
