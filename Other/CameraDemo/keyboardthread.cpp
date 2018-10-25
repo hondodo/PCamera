70ef17ff20b6c0b755e052336c6219205f7ba27b
hondodo <potatokidh@gmail.com>
2018-10-25 08:25:23 +0800

    test ring debug tip

--- Other/CameraDemo/keyboardthread.cpp
+++ Other/CameraDemo/keyboardthread.cpp
@@ -56,7 +56,7 @@ void KeyBoardThread::run()
         {
             if(digitalRead(P0) == 0)
             {
-                if(ringKeyOnCount > 2)//至少持续2*20=40ms
+                if(ringKeyOnCount >= 2)//至少持续2*20=40ms
                 {
                     lastCheckRing = now;
                     emit onKey(0);
